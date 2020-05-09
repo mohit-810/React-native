fa8eeda440834e6cec57f327680215feb183ea82
mohit <mohitsachdeva702@gmail.com>
2020-05-10 00:29:32 +0530

    dishes

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Main from './components/MainComponents';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Main />
+    );
+  }
+}
